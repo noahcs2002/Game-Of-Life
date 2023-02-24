62ef70f34ad30e90161cb6d7f4cdbb568fa49426
noahcs2002 <84613649+noahcs2002@users.noreply.github.com>
2023-02-23 22:57:13 -0600

    Settings file made

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 
 import copy, random, sys, time
+from settings import *
 
-WIDTH = 79 #=sys.argv[1]
-HEIGHT = 20 #=sys.argv[2] 
-
-ALIVE = '|'  
-DEAD = '-'  
-nextCells = {}
-aliveCount = 0
-stages = 0
-isStagnantCount = 0
-stagCount = 5
 
 class Extinction(Exception): pass
 class IsolatedSystem(Exception) : pass
---
+++ settings.py
@@ -0,0 +1,10 @@
+WIDTH = 79 #=sys.argv[1]
+HEIGHT = 20 #=sys.argv[2] 
+
+ALIVE = '|'  
+DEAD = '-'  
+nextCells = {}
+aliveCount = 0
+stages = 0
+isStagnantCount = 0
+stagCount = 5
