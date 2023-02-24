3faeda1e92f40f1dfbccc744dc3e1af7461973de
noahcs2002 <84613649+noahcs2002@users.noreply.github.com>
2023-02-23 22:48:38 -0600

    updates

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ nextCells = {}
 aliveCount = 0
 stages = 0
 isStagnantCount = 0
+stagCount = 5
 
 class Extinction(Exception): pass
 class IsolatedSystem(Exception) : pass
@@ -24,7 +25,7 @@ for x in range(WIDTH):
 try:
     while True:
         print('\n' * 25) 
-        print(f'Current Iteration: Alive = {ALIVE}, Dead = {DEAD} AliveCount: {aliveCount}, Steps to Stagnation: {25 - isStagnantCount}')
+        print(f'Current Iteration: Alive = {ALIVE}, Dead = {DEAD}, AliveCount: {aliveCount}, Steps to Stagnation: {stagCount - isStagnantCount}')
         cells = copy.deepcopy(nextCells)
         aliveOldCount = aliveCount
         aliveCount = 0
@@ -46,7 +47,7 @@ try:
         else:
             isStagnantCount = 0
             
-        if isStagnantCount > 25:
+        if isStagnantCount > stagCount:
             raise(IsolatedSystem)
 
         for x in range(WIDTH):
@@ -94,7 +95,7 @@ try:
             print(f"Conway's Game of Life: Simulation Stages Ran: {stages}")
             sys.exit()  
 except IsolatedSystem:
-    print(f'Isolated System Became Sustainable After {stages} Simulation Stages')
+    print(f'Isolated System(s) of total size {aliveCount} Became Sustainable After {stages} Simulation Stages')
     sys.exit()  
 except Extinction:
     print(f'Colony Has Gone Extinct After {stages} Simulation Stages')
